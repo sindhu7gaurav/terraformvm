2ebe4c2a3d549e64342a18fce9fe3adaa6761bea
sindhu7gaurav <sindhu7gaurav@gmail.com>
2026-04-13 21:21:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project     = "diesel-equator-491908-q2"   # 👈 replace with your real project ID
+  project     = "prabhat-new-project-2026"   # 👈 replace with your real project ID
   region      = "us-central1"
   credentials = var.gcp_credentials
 }
@@ -10,14 +10,14 @@ variable "gcp_credentials" {
 
 # Enable required APIs
 resource "google_project_service" "compute_api" {
-  project = "diesel-equator-491908-q2"
+  project = "prabhat-new-project-2026"
   service = "compute.googleapis.com"
 
   disable_on_destroy = false
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-vm"
+  name         = "terraform-testvm"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
