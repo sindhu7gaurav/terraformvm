c815a0c360d6e089f8513d9e677fa8b979797983
sindhu7gaurav <sindhu7gaurav@gmail.com>
2026-04-14 15:10:05 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+  }
+
+  provider "google" {
+  region = var.region
+}
