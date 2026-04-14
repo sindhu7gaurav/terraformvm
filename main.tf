949d4cb4acc928659699115d525271ead8d3ddea
sindhu7gaurav <sindhu7gaurav@gmail.com>
2026-04-14 21:20:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,42 @@
 resource "google_compute_instance" "vm" {
   name         = var.name
   project      = var.project_id
-  machine_type = var.os_type == "windows" ? "e2-medium" : "e2-micro"
-  zone         = "asia-south1-c"
+  machine_type = var.machine_type
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
       image = var.os_type == "windows" ? "windows-cloud/windows-2022" : "debian-cloud/debian-12"
+      size  = 50
     }
   }
-network_interface {
-    network    = var.network_link
-    subnetwork = var.subnetwork_link
+
+  network_interface {
+    network            = var.network_link
+    subnetwork         = var.subnetwork_link
+    network_project    = var.host_project_id
+    access_config {}
   }
+
   labels = {
     patch_group = "production"
     backup      = "daily"
   }
+
+  tags = ["http-server", "https-server"]
+}
+
+output "instance_self_link" {
+  description = "The self link of the created instance"
+  value       = google_compute_instance.vm.self_link
+}
+
+output "instance_internal_ip" {
+  description = "The internal IP address of the instance"
+  value       = google_compute_instance.vm.network_interface[0].network_ip
+}
+
+output "instance_external_ip" {
+  description = "The external IP address of the instance"
+  value       = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
 }
